de29ac03ab49bd3d6ff9d8a76088f7062ed89f44
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2026-04-28 19:55:31 -0300

    test: add examples and output validation for various validation functions

--- example_test.go
+++ example_test.go
@@ -4,8 +4,27 @@ import (
 	"fmt"
 
 	mv "github.com/Multiform-Validator/go"
+	"github.com/Multiform-Validator/go/email"
 	"github.com/Multiform-Validator/go/validate"
 )
+
+func ExampleIsCPF() {
+	fmt.Println(mv.IsCPF("123.456.789-09") == nil)
+	fmt.Println(mv.IsCPF("123.456.789-02") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsCNPJ() {
+	fmt.Println(mv.IsCNPJ("04.252.011/0001-10") == nil)
+	fmt.Println(mv.IsCNPJ("12.345.678/0001-91") == nil)
+
+	// Output:
+	// true
+	// false
+}
 
 func ExampleIsEmail() {
 	fmt.Println(mv.IsEmail("user@example.com") == nil)
@@ -23,6 +42,26 @@ func ExampleGetOnlyEmail() {
 	// joao@empresa.com
 }
 
+func ExampleGetOnlyEmails() {
+	emails := mv.GetOnlyEmails(
+		"Contact: john@gmail.comXTRA, alexa@gmail.comXTRA",
+		email.GetOnlyEmailOptions{CleanDomain: true, RepeatEmail: true},
+	)
+	fmt.Println(emails)
+
+	// Output:
+	// [john@gmail.com alexa@gmail.com]
+}
+
+func ExampleIsCreditCard() {
+	fmt.Println(mv.IsCreditCard("4111 1111 1111 1111") == nil)
+	fmt.Println(mv.IsCreditCard("4111 1111 1111 1112") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
 func ExampleIdentifyFlagCard() {
 	fmt.Println(mv.IdentifyFlagCard("4111 1111 1111 1111"))
 	fmt.Println(mv.IdentifyFlagCard("7000 0000 0000 0000"))
@@ -30,6 +69,86 @@ func ExampleIdentifyFlagCard() {
 	// Output:
 	// Visa
 	// Unknown
+}
+
+func ExampleIsTelephone() {
+	fmt.Println(mv.IsTelephone("+55 11 91234-5678", "BR") == nil)
+	fmt.Println(mv.IsTelephone("+91 51234 56789", "IN") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsBase64() {
+	fmt.Println(mv.IsBase64("SGVsbG8gV29ybGQ=") == nil)
+	fmt.Println(mv.IsBase64("SGVs bG8=") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsMD5() {
+	fmt.Println(mv.IsMD5("d41d8cd98f00b204e9800998ecf8427e") == nil)
+	fmt.Println(mv.IsMD5("d41d8cd98f00b204e9800998ecf8427g") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsCEP() {
+	fmt.Println(mv.IsCEP("12345-678") == nil)
+	fmt.Println(mv.IsCEP("12345.678") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsPostalCode() {
+	fmt.Println(mv.IsPostalCode("SW1A 1AA", "UK") == nil)
+	fmt.Println(mv.IsPostalCode("90210-123", "US") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsPort() {
+	fmt.Println(mv.IsPort("8080") == nil)
+	fmt.Println(mv.IsPort("65536") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsBlank() {
+	fmt.Println(mv.IsBlank(" \t\n") == nil)
+	fmt.Println(mv.IsBlank("value") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleIsAscii() {
+	fmt.Println(mv.IsAscii("Hello 123!") == nil)
+	fmt.Println(mv.IsAscii("olá") == nil)
+
+	// Output:
+	// true
+	// false
+}
+
+func ExampleCalculateCNPJCheckDigits() {
+	digits, err := mv.CalculateCNPJCheckDigits("12ABC34501DE")
+	fmt.Println(digits, err == nil)
+
+	// Output:
+	// 35 true
 }
 
 func Example_validateEmail() {
