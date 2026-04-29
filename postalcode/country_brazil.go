b70ad5d53cdc1df777ca10badf17d3415f93d61a
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2026-04-28 21:21:13 -0300

    refactor: simplify Brazil postal code validation by using cep package

--- postalcode/country_brazil.go
+++ postalcode/country_brazil.go
@@ -1,16 +1,11 @@
 package postalcode
 
+import "github.com/Multiform-Validator/go/cep"
+
 var brazilRule = countryRule{
 	validate: isBrazilPostalCode,
 }
 
 func isBrazilPostalCode(value string) bool {
-	if len(value) == 8 {
-		return hasOnlyDigits(value)
-	}
-
-	return len(value) == 9 &&
-		hasOnlyDigits(value[:5]) &&
-		value[5] == '-' &&
-		hasOnlyDigits(value[6:])
+	return cep.IsCEP(value) == nil
 }
